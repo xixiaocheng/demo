53e1cdf5bfd3e6c44b0a3f3e7b44431e6099b345
奚皛呈 <xixiaocheng@bilibili.com>
2022-08-29 22:02:28 +0800

    refactor: 💡 一些调整

--- package/v2/vite.config.js
+++ package/v2/vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from "vite";
 import { createVuePlugin } from "vite-plugin-vue2";
 import vue3 from "@vitejs/plugin-vue";
 import * as compiler from "@vue/compiler-sfc";
-import { isVue2 } from "vue-demi";
+import { isVue2, version } from "vue-demi";
 
-console.log(isVue2)
+console.log(`当前的打包环境为vue ${version}`)
 import path from "path";
 const resolve = (str) => {
   return path.resolve(__dirname, str);
@@ -15,6 +15,7 @@ export default defineConfig({
     outDir: resolve("lib"),
     lib: {
       entry: resolve("main.js"),
+      name: "test",
     },
     rollupOptions: {
       external: ["vue-demi", "vue"],
